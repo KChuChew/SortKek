a4d093677f7326597f758448c8a57d1ec4960d1a
Kevin Chu <kevin.git.chu@gmail.com>
2018-05-25 13:40:47 -0700

    added insertion sort

--- sort.cpp
+++ sort.cpp
@@ -67,7 +67,6 @@ int main(int argc, char * argv[]) {
     }
   }
  
-  std::cout << quantity + 5 << "\n";
   s.clear();
 
   unsigned int limit = 0; // var to store upper bound on integer values
@@ -98,76 +97,80 @@ int main(int argc, char * argv[]) {
     int r = std::rand() % limit;
     to_sort.push_back(r);
   }
-  /*for(unsigned int i = 0; i < to_sort.size(); ++i) {
-std::cout << to_sort[i] << " ";
-  }*/
-//std::cout << "\n";
-	while(true) {
-  	std::cout << "Sort with which algorithm? :\n" 
-                 "bubble sort (b),\n" 
-                 "selection sort (s),\n"
-                 "quick sort (q),\n" 
-                 "merge sort (m),\n" 
-                 "insertion sort (i),\n" 
-                 "OR type exit when done testing.\n"; 
-  	//std::cin >> choice;
-  	std::cin >> choice;
-		std::cout << choice << "\n"; 
- 		if(choice == "exit") {
-      break;
-    }
-    if(choice.length() > 1) {
+  while(1) {
+  std::cout << "Sort with which algorithm? :\n" 
+               "bubble sort (b),\n" 
+               "selection sort (s),\n"
+               "quick sort (q),\n" 
+               "merge sort (m),\n" 
+               "insertion sort (i),\n" 
+               "OR type exit when done testing.\n"; 
+ 	//std::cin >> choice;
+ 	std::cin >> choice;
+	std::cout << choice << "\n"; 
+ 	if(choice == "exit") {
+    break;
+  }
+  if(choice.length() > 1) {
+    std::cout << "Please enter valid option\n";
+    continue;
+  }
+  if(choice.length() == 1) {
+    char c = choice[0];
+std::cerr << "CHOICE NOT JUDGED " << c << "\n";
+    if(c != 'b' && c != 's' && c != 'q' && c != 'm' && c != 'i') {
       std::cout << "Please enter valid option\n";
       continue;
     }
-    if(choice.length() == 1) {
-      char c = choice[0];
-std::cerr << "CHOICE NOT JUDGED " << c << "\n";
-      if(c != 'b' && c != 's' && c != 'q' && c != 'm' && c != 'i') {
-        std::cout << "Please enter valid option\n";
-        continue;
-      }
-    }
-    char c = choice[0];
+  }
+  char c = choice[0];
 std::cerr << "CORRECT CHOICE " << c << "\n";
-    //choice = std::stoi(choice);	
-	
-  	switch (c) {
-    	case 'b': {
-      	//to_sort = bubble_sort(to_sort);
-        std::cout << "Begin Bubble Sorting\n";
-        auto start = std::chrono::high_resolution_clock::now();
-        std::vector<int> output = bubble_sort(to_sort);
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto dur = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "Time taken by Bubble Sort: " << dur.count() << " milliseconds.\n";
-        std::cout << "Output Vector \n";
-        /*for(unsigned int i = 0; i < output.size(); ++i) {
-          std::cout << output[i] << " "; 
-        }
-        std::cout << "\n";*/
-        break;
+  //choice = std::stoi(choice);	
+
+	switch (c) {
+   	case 'b': {
+      std::cout << "Begin Bubble Sorting\n";
+      auto start = std::chrono::high_resolution_clock::now();
+      std::vector<int> output = bubble_sort(to_sort);
+      auto stop = std::chrono::high_resolution_clock::now();
+      auto dur = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+      std::cout << "Time taken by Bubble Sort: " << dur.count() << " milliseconds.\n";
+      std::cout << "Output Vector \n";
+      for(unsigned int i = 0; i < output.size(); ++i) {
+        std::cout << output[i] << " "; 
       }
-    	case 's':
-      	std::cout << "Begin Selection Sorting\n";
-        break;
+      std::cout << "\n";
+      break;
+    }
+  	case 's':
+    	std::cout << "Begin Selection Sorting\n";
+      break;
    
-    	case 'q':
-        std::cout << "Begin Quick Sorting\n";	
-        break;
+    case 'q':
+      std::cout << "Begin Quick Sorting\n";	
+      break;
    
-    	case 'm': 
-      	std::cout << "Begin Merge Sorting\n";
-        break;
+    case 'm': 
+    	std::cout << "Begin Merge Sorting\n";
+      break;
    
-    	case 'i':
-      	std::cout << "Begin Insertion Sorting\n";
-        break;
-   
-    	default:
-				std::cout << "Please choose a sorting algorithm from above\n";  
-		}
+    case 'i': {
+      std::cout << "Begin Insertion Sorting\n";
+      auto start = std::chrono::high_resolution_clock::now();
+      std::vector<int> output = insertion_sort(to_sort);
+      auto stop = std::chrono::high_resolution_clock::now();
+      auto dur = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+      std::cout << "Time taken by Insertion Sort: " << dur.count() << " milliseconds.\n";
+      std::cout << "Output Vector \n";
+      for(unsigned int i = 0; i < output.size(); ++i) {
+        std::cout << output[i] << " "; 
+      }
+      std::cout << "\n";
+      break;
+    } 
+    default:
+			std::cout << "Please choose a sorting algorithm from above\n";  
 	}
-  
+  }  
   return 0;
 }
--- sort_algos.cpp
+++ sort_algos.cpp
@@ -1,4 +1,5 @@
 /*
+ * Info: Kevin Chu : 25 May 2018 : 牛肉 
  * Name: sort_algos.cpp
  *
  */
@@ -8,13 +9,10 @@
  * Bubble Sort: O(n^2)
  */
 std::vector<int> bubble_sort(std::vector<int> to_sort) {
-
-  int holder = 0;
-
   for(int j = 0; j < to_sort.size() - 1; j++) {  
-    for(int i = 0; i < to_sort.size() - j -1; i++) {
+    for(int i = 0; i < to_sort.size() - j - 1; i++) {
       if(to_sort[i] > to_sort[i+1]) {
-        holder = to_sort[i];
+        int holder = to_sort[i];
 	      to_sort[i] = to_sort[i+1];
 	      to_sort[i+1] = holder;
       }
@@ -28,23 +26,32 @@ std::vector<int> bubble_sort(std::vector<int> to_sort) {
  * Insertion Sort: O(n^2)
  */
 std::vector<int> insertion_sort(std::vector<int> to_sort) {
+  for(int i = 0; i < to_sort.size(); ++i) {
+    std::cout << to_sort[i] << " ";
+  }
+  std::cout << "\n";
+  for(int i = 1; i < to_sort.size(); ++i) {
+    int counter = i - 1;
+    int ele = to_sort[i];
+    while(counter >= 0 && ele < to_sort[counter]) {
+      to_sort[counter + 1] = to_sort[counter];
+      counter--; 
+    }
+    to_sort[counter + 1] = ele; 
+  }
 
+  return to_sort;
+}
+
+/*
+ * Merge Sort: O(nlogn)
+ */
+std::vector<int> merge_sort(std::vector<int> to_sort) {
+  return to_sort;
 }
 
 /*
  * Selection Sort: O(n^2)
  */
 std::vector<int> selection_sort(std::vector<int> to_sort) {
-}
-
-
-/*
- * Merge Sort: O(nlogn)
- */
-std::vector<int> merge_sort(std::vector<int> to_sort) {
-
-
-
-
-  return to_sort;
 }
